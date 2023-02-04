95108d99116cf5160aa828c02b0373f922c90684
Tahiya Anjum Tasnim <122308843+tahiya252@users.noreply.github.com>
2023-02-04 18:06:49 +0600

    Add files via upload

---
+++ Lab-04/Petya and Strings.cpp
@@ -0,0 +1,35 @@
+//Problem name-Petya and Strings
+//Problem no-112A
+
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string a,b;
+    int i;
+    cin>>a>>b;
+    for(i=0;i<a.size();i++)
+    {
+        a[i]=towlower(a[i]);
+        b[i]=towlower(b[i]);
+    }
+    if (a==b)
+        cout<<"0"<<endl;
+    else
+    {
+        for(i=0;i<a.size();i++)
+        {
+            if (a[i]<b[i])
+            {
+                cout<<"-1"<<endl;
+                break;
+            }
+            if (a[i]>b[i])
+            {
+                cout<<"1"<<endl;
+                break;
+            }
+        }
+    }
+}
