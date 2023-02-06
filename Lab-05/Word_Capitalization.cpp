777eff75a8e33e0d84947935aab524d5ff6656ea
Tahiya Anjum Tasnim <122308843+tahiya252@users.noreply.github.com>
2023-02-06 22:03:35 +0600

    Add files via upload

---
+++ Lab-05/Word_Capitalization.cpp
@@ -0,0 +1,14 @@
+//Problem name-Word Capitalization
+//Problem no-281A
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    s[0]=towupper(s[0]);
+    cout<<s;
+    return 0;
+}
